1212e7f8524429743383172bbd0a117922c199ed
Aryan123-rgb <srivastavaaryanalc76@gmail.com>
2025-07-22 20:57:33 +0530

    added xelatex compiler

--- src/lib/latex.ts
+++ src/lib/latex.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { exec } from "child_process";
 import fs from "fs/promises";
 
+
 export const generatePDF = async (latex_code: string) => {
     const folderPath = path.join(process.cwd(), "tmp");
     const texFilePath = path.join(folderPath, "resume.tex");
@@ -10,43 +11,64 @@ export const generatePDF = async (latex_code: string) => {
     await fs.mkdir(folderPath, { recursive: true });
     await fs.writeFile(texFilePath, latex_code);
 
-    console.log("latex_code from latex.ts", latex_code);
+    // remove the pdf if already exists
+    await fs.rm(pdfPath, { force: true });
+
+    // console.log("latex_code from latex.ts", latex_code);
 
     // Make folderPath POSIX for Docker mount (important on Windows)
     const folderPathPosix = folderPath.replace(/\\/g, "/");
-    const dockercmd = [
-        "docker run --rm",
-        `-v "${folderPathPosix}:/data"`,
-        "blang/latex",
-        "pdflatex -interaction=nonstopmode resume.tex"
-    ].join(" ");
 
-    await new Promise<void>((resolve, reject) => {
+    const compileResume = async (compiler: "xelatex" | "pdflatex") => {
+        const dockercmd = [
+            "docker run --rm",
+            `-v "${folderPathPosix}:/data"`,
+            "blang/latex",
+            `${compiler} -interaction=nonstopmode resume.tex`
+        ].join(" ");
 
-        exec(dockercmd, { cwd: folderPath }, async (err, stdout, stderr) => {
-            const pdfExists = await fs.access(pdfPath).then(() => true).catch(() => false);
+        await new Promise<void>((resolve, reject) => {
+            console.log("executing ", dockercmd);
+            exec(dockercmd, { cwd: folderPath }, async (err, stdout, stderr) => {
+                const pdfExists = await fs.access(pdfPath).then(() => true).catch(() => false);
+                console.log("pdfExists", pdfExists);
 
-            const hasLatexError = stderr.includes('! LaTeX Error:') || stdout.includes('! LaTeX Error:');
+                if (!pdfExists) {
+                    if (compiler == 'pdflatex') {
+                        console.error("Docker execution failed:", err);
+                        console.error("stderr:", stderr);
+                        console.error("stdout:", stdout);
+                    }
 
-            if (err || !pdfExists || hasLatexError) {
-                console.error("Docker execution failed:", err);
-                console.error("stderr:", stderr);
-                console.error("stdout:", stdout);
+                    let errorMessage = "Failed to compile the LaTeX code.";
 
-                let errorMessage = "Failed to compile the LaTeX code.";
-                if (hasLatexError) {
-                    const lines = stderr.split('\n').concat(stdout.split('\n'));
-                    const latexErrors = lines.filter(line => line.includes('!'));
-                    errorMessage += "\n\nDetails:\n" + latexErrors.join('\n');
+                    return reject(new Error(errorMessage));
                 }
 
-                return reject(new Error(errorMessage));
-            }
+                resolve();
+            });
 
-            resolve();
         });
+    }
 
-    });
+    try {
+        await compileResume('xelatex');
+    } catch (error) {
+        try {
+            const stdoutPath = path.join(folderPath, 'resume.out');
+            const logPath = path.join(folderPath, 'resume.log');
+            await fs.rm(stdoutPath, { force: true });
+            await fs.rm(logPath, { force: true });
+            console.log("executing pdflatex compiler");
+            await compileResume('pdflatex');
+        } catch (e) {
+            console.log("compilation failed");
+            throw new Error("Compilation failed");
+        }
+    }
+
+    // await fs.rm(texFilePath, { force: true }).catch(() => { });
+    // await fs.rm(pdfPath, { force: true }).catch(() => { });
 
     const pdfBuffer = await fs.readFile(pdfPath);
 
